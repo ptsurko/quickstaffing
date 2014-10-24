5017ca27f0e2535def47db6e5ea50e5e35585507
dulasau <dzmitry.ulasau@gmail.com>
2014-10-24 06:19:41 -0700

    update UI

--- public/js/controllers/indexcontroller.js
+++ public/js/controllers/indexcontroller.js
@@ -3,14 +3,14 @@ var IndexController = function(e3sservice) {
 
   this.stage_ = new Kinetic.Stage({
     container: 'canvas-container',
-    width: 1012,
-    height: 360
+    width: 1020,
+    height: 300
   });
   this.layer_ = new Kinetic.Layer();
 
   this.positions = [];
   this.candidates = [];
-  this.selectedCandidate_ = 8;
+  this.selectedCandidate_ = 3;
 
   this.loadCandidates_();
   this.loadPositions_();
@@ -26,21 +26,23 @@ IndexController.prototype.loadPositions_ = function() {
 
 IndexController.prototype.onCandidatesLoaded_ = function(response) {
   this.candidates = response;
+  console.log(response);
 };
 
 IndexController.prototype.onPositionsLoaded_ = function(response) {
   this.positions = response;
+  console.log(response);
 };
 
 IndexController.prototype.matchCandidates = function(position, $event, $index) {
   var elementWidth = $event.srcElement.clientWidth;
-  var x1 = ($index * (elementWidth + 2) + elementWidth / 2);
-  var x2 = (this.selectedCandidate_ * (elementWidth + 2) + elementWidth / 2);
+  var x2 = ($index * (elementWidth + 2) + elementWidth / 2);
+  var x1 = (this.selectedCandidate_ * (elementWidth + 2) + elementWidth / 2);
+
 
 // Do not remove it!!!
-// var x2 = ($index * (elementWidth + 2) + elementWidth / 2);
-// var x1 = (driveToIndex * (elementWidth + 2) + elementWidth / 2);
-
+// var x1 = ($index * (elementWidth + 2) + elementWidth / 2);
+// var x2 = (this.selectedCandidate_ * (elementWidth + 2) + elementWidth / 2);
   this.drawLine_(x1, x2);
 };
 
@@ -50,7 +52,7 @@ IndexController.prototype.selectCandidate = function($index) {
 
 IndexController.prototype.drawLine_ = function(x1, x2) {
   var y1 = 0;
-  var y2 = 350;
+  var y2 = 300;
   var a = (y2 - y1) / (x2 - x1);
   var b = y2 - a * x2;
   var points = [x1, y1];
