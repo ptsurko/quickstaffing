97cbdf67c1e22e2df9e4a5bcb326702dbe584558
dulasau <dzmitry.ulasau@gmail.com>
2014-10-24 04:34:44 -0700

    Updated Index controller

--- public/js/controllers/indexcontroller.js
+++ public/js/controllers/indexcontroller.js
@@ -1,14 +1,13 @@
-angular.module('QuickStaffing', [])
-  .controller('IndexController', ['$scope', 'e3sservice', function($scope, e3sservice) {
-    e3sservice.getCandidates(function() {
-      debugger
-    });
-  }]);
-
-
 var IndexController = function($scope, $http) {
   this.scope_ = $scope;
   this.http_ = $http;
+
+  this.stage_ = new Kinetic.Stage({
+    container: 'container',
+    width: 1012,
+    height: 360
+  });
+  this.layer_ = new Kinetic.Layer();
 
   this.candidates = [{
     fullName: 'Dzmitry Ulasau'
@@ -83,20 +82,15 @@ IndexController.prototype.matchCandidates = function(position, $event, $index) {
 //        var x2 = ($index * (elementWidth + 2) + elementWidth / 2);
 //        var x1 = (driveToIndex * (elementWidth + 2) + elementWidth / 2);
 
-  drawLine(x1, x2);
+  this.drawLine_(x1, x2);
 };
 
 IndexController.prototype.selectCandidate = function($index) {
   driveToIndex = $index;
 };
 
-var driveToIndex = 9;
-var stage = new Kinetic.Stage({
-  container: 'container',
-  width: 1012,
-  height: 360
-});
-var layer = new Kinetic.Layer();
+var driveToIndex = 8;
+
 
 
 function getRandomInt(min, max) {
@@ -107,23 +101,18 @@ function getRandom(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-var drawLine = function(x1, x2) {
+IndexController.prototype.drawLine_ = function(x1, x2) {
   var y1 = 0;
   var y2 = 350;
-
   var a = (y2 - y1) / (x2 - x1);
   var b = y2 - a * x2;
   var points = [x1, y1];
+  var point = this.calcX_(a, b, getRandomInt(60, 100)) - getRandomInt(80, 160);
 
-  var point = calcX(a, b, getRandomInt(60, 100)) - getRandomInt(80, 160);
-  console.log(calcX(a, b, getRandomInt(60, 100)) - getRandomInt(80, 160));
-  console.log(point);
   points.push(point);
   points.push(getRandomInt(120, 160));
-
-  points.push(calcX(a, b, getRandomInt(180, 220)) + getRandomInt(80, 160));
+  points.push(this.calcX_(a, b, getRandomInt(180, 220)) + getRandomInt(80, 160));
   points.push(getRandomInt(180, 220));
-
   points.push(x2); points.push(y2);
 
   var blueSpline = new Kinetic.Line({
@@ -134,12 +123,8 @@ var drawLine = function(x1, x2) {
     tension: 0.5
   });
 
-  layer.add(blueSpline);
-  stage.add(layer);
-};
-
-var calcY = function(a, b, x) {
-  return a * x + b;
+  this.layer_.add(blueSpline);
+  this.stage_.add(this.layer_);
 };
 
 var calcX = function(a, b, y) {
@@ -148,3 +133,13 @@ var calcX = function(a, b, y) {
   }
   return (y - b) / a;
 };
+
+IndexController.prototype.calcX_ = function(a, b, y) {
+  if (a == Infinity) {
+    return y;
+  }
+  return (y - b) / a;
+};
+
+angular.module('QuickStaffing', [])
+    .controller('IndexController', ['$scope', 'e3sservice', IndexController]);
