1956cfb9785396fecdd9aeedc417b4ebf6bbab26
dulasau <dzmitry.ulasau@gmail.com>
2014-10-24 15:51:45 -0700

    +10

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-angular.module('QuickStaffing', ['ngSlider', 'siyfion.sfTypeahead']).
+angular.module('QuickStaffing', ['ngSlider','ngDialog', 'siyfion.sfTypeahead']).
     service('e3sservice', E3SService).
     service('rankservice', RankService).
     service('positionservice', PositionService).
